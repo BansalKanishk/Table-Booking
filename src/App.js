71fd4250d84bd54dd4e2316b3b908c6ea1de4ab1
Kanishk Bansal <101380013+BansalKanishk@users.noreply.github.com>
2023-02-02 15:44:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Nav, Main, Footer } from './template'
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
